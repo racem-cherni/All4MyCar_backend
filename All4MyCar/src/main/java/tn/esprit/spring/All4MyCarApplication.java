0c78342c9390fdfe93d7419e9a803d3c3be2bebe
Racem Cherni <racem.cherni@esprit.tn>
2020-09-10 10:46:09 +0100

    update files

---
+++ All4MyCar/src/main/java/tn/esprit/spring/All4MyCarApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class All4MyCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(All4MyCarApplication.class, args);
+	}
+
+}
